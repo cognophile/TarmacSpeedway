2a6e119f19281d51f8ab8765be151c9c346f83d5
cognophile <cognophile@users.noreply.github.com>
2018-02-04 00:32:08 +0000

    Update to format splash components

--- Part-A/src/main/java/Main.java
+++ Part-A/src/main/java/Main.java
@@ -11,25 +11,33 @@ public class Main {
         CoreWindow splash = new CoreWindow("TarmacSpeedway: Pits Turntable!");
         splash.setWindowSize(500, 600);
 
+        // Create the supplementary panels
         JPanel mainPanel = new JPanel();
-
-        // Create the header label
-        HeaderPanel title = new HeaderPanel("Will this do the job?");
-        title.setLayout(new FlowLayout());
+        JButton startGameButton = new JButton("Let's Race!");
+        HeaderPanel titleLabel = new HeaderPanel("Welcome!");
 
         // Create the panel that will host the images
-        TurntablePanel animator = new TurntablePanel();
-        animator.setAnimationSpeed(1000);
+        TurntablePanel animationPanel = new TurntablePanel();
+        animationPanel.setAnimationSpeed(1000);
 
-        // Create the 'start game' button
-        JButton startGame = new JButton("Race!");
+        // Construct the panels into a vertical layout
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
-        // Add the image panel to the window, and render the window.
-        splash.addComponents(title);
-        splash.addComponents(startGame);
-        splash.addComponents(animator);
+        titleLabel.setMaximumSize( new Dimension(600, 50));
+        startGameButton.setMaximumSize( new Dimension(120, 30));
+        animationPanel.setMaximumSize( new Dimension(600, 500));
 
-        // Render the window frame
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        startGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        animationPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Add panel elements to a container panel
+        mainPanel.add(titleLabel);
+        mainPanel.add(animationPanel);
+        mainPanel.add(startGameButton);
+
+        // Add the container to the window frame and render
+        splash.addComponents(mainPanel);
         splash.render();
     }
 }
