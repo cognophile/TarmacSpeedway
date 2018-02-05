19769b6655cf83a33bf990b7436eafb44ce59d60
Cognophile <cognophile@users.noreply.github.com>
2018-02-05 20:59:25 +0000

    Add independant car colour loaders

--- Part-A/src/main/java/ImageLoader.java
+++ Part-A/src/main/java/ImageLoader.java
@@ -7,28 +7,29 @@ import java.util.Map;
 
 public abstract class ImageLoader
 {
-    private static final String RESOURCE_IMAGES_UNIX = "/src/main/resources/images/red";
-    private static final String RESOURCE_IMAGES_WINDOWS = "\\src\\main\\resources\\images\\red";
+    private static final String RESOURCE_RED_CAR_IMAGES_UNIX = "/src/main/resources/images/red";
+    private static final String RESOURCE_GREEN_CAR_IMAGES_UNIX = "/src/main/resources/images/green";
+    private static final String RESOURCE_RED_CAR_IMAGES_WINDOWS = "\\src\\main\\resources\\images\\red";
+    private static final String RESOURCE_GREEN_CAR_IMAGES_WINDOWS = "\\src\\main\\resources\\images\\green";
 
     /**
-     * Import the images found in 'resources\images\red'
+     * Import the images found in 'resources\images\green', independent of platform.
      * @throws if the device OS is not Unix-based or Windows, throw an UnsupportedOperationException
      * @return Map of image filesnames (Strings) to a representation of the corresponding image (ImageIcon)
      */
-    public static Map<String, ImageIcon> importImages() throws UnsupportedOperationException
+    public static Map<String, ImageIcon> loadRedCar() throws UnsupportedOperationException
     {
         File imageDirectory = null;
-        Map<String, ImageIcon> iconMap = new HashMap<>();
-        String path = new File("").getAbsolutePath();
+        Map<String, ImageIcon> carImageMap = new HashMap<>();
 
         if (DeviceOS.isUnix() || DeviceOS.isMac())
         {
-            imageDirectory = new File(path + RESOURCE_IMAGES_UNIX);
+            imageDirectory = ImageLoader.getRedCarImagePath(RESOURCE_RED_CAR_IMAGES_UNIX);
         }
 
         if (DeviceOS.isWindows())
         {
-            imageDirectory = new File(path + RESOURCE_IMAGES_WINDOWS);
+            imageDirectory = ImageLoader.getRedCarImagePath(RESOURCE_RED_CAR_IMAGES_WINDOWS);
         }
 
         if (imageDirectory == null)
@@ -41,11 +42,61 @@ public abstract class ImageLoader
             {
                 if(file.getName().toLowerCase().endsWith(".png"))
                 {
-                    iconMap.put(file.getName(), new ImageIcon(file.getPath()));
+                    carImageMap.put(file.getName(), new ImageIcon(file.getPath()));
                 }
             }
         }
 
-        return iconMap;
+        return carImageMap;
+    }
+
+    /**
+     * Import the images found in 'resources\images\green', independent of platform.
+     * @throws if the device OS is not Unix-based or Windows, throw an UnsupportedOperationException
+     * @return Map of image filesnames (Strings) to a representation of the corresponding image (ImageIcon)
+     */
+    public static Map<String, ImageIcon> loadGreenCar() throws UnsupportedOperationException
+    {
+        File imageDirectory = null;
+        Map<String, ImageIcon> carImageMap = new HashMap<>();
+
+        if (DeviceOS.isUnix() || DeviceOS.isMac())
+        {
+            imageDirectory = ImageLoader.getGreenCarImagePath(RESOURCE_GREEN_CAR_IMAGES_UNIX);
+        }
+
+        if (DeviceOS.isWindows())
+        {
+            imageDirectory = ImageLoader.getGreenCarImagePath(RESOURCE_GREEN_CAR_IMAGES_WINDOWS);
+        }
+
+        if (imageDirectory == null)
+        {
+            throw new UnsupportedOperationException("This operating system is not supported.");
+        }
+        else
+        {
+            for (File file : imageDirectory.listFiles())
+            {
+                if(file.getName().toLowerCase().endsWith(".png"))
+                {
+                    carImageMap.put(file.getName(), new ImageIcon(file.getPath()));
+                }
+            }
+        }
+
+        return carImageMap;
+    }
+
+    private static File getRedCarImagePath(String resourcePath)
+    {
+        String systemPath = new File("").getAbsolutePath();
+        return new File(systemPath + resourcePath);
+    }
+
+    private static File getGreenCarImagePath(String resourcePath)
+    {
+        String systemPath = new File("").getAbsolutePath();
+        return new File(systemPath + resourcePath);
     }
 }
